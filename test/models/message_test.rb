1013c54e248649061d286265036079b019834da9
Malcolm Jeremiah Rajeli <malcolmjeremiahrajeli@gmail.com>
2020-10-27 09:49:16 +0800

    Message test

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "message have id uuid type" do
+    assert = Message.columns.find{|col| col.name == 'id'}.sql_type == 'uuid'
+  end
+
+  test "raise error if null to column" do
+    assert_raises(ActiveRecord::NotNullViolation) {
+      message = messages(:message_one)
+      message.to = nil
+      message.save!
+    }
+  end
+
+  test "raise error if null from column" do
+    assert_raises(ActiveRecord::NotNullViolation) {
+      message = messages(:message_one)
+      message.from = nil
+      message.save!
+    }
+  end
+
+  test "raise error if null body column" do
+    assert_raises(ActiveRecord::NotNullViolation) {
+      message = messages(:message_one)
+      message.body = nil
+      message.save!
+    }
+  end
+
+  test "raise error if null twilio response column" do
+    assert_raises(ActiveRecord::NotNullViolation) {
+      message = messages(:message_one)
+      message.twilio_response = nil
+      message.save!
+    }
+  end
+
+  test "twilio_response default to empty hash" do
+    assert messages(:message_one).twilio_response == {}
+  end
 end
