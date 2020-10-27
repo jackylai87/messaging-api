83f59cf0ee5ae5931b1616c8aa9b035486bd4f3f
Malcolm Jeremiah Rajeli <malcolmjeremiahrajeli@gmail.com>
2020-10-27 11:57:04 +0800

    Conversation test

--- test/models/conversation_test.rb
+++ test/models/conversation_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class ConversationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @messenger = Message.new(
+      to: 'messenger:102542248309018',
+      from: 'messenger:4465012966906978',
+      body: 'Hello There'
+    )
+
+    @whatsapp = Message.new(
+      to: 'whatsapp:+60145586061',
+      from: 'whatsapp:+60145586061',
+      body: 'Hello There'
+    )
+
+    @sms = Message.new(
+      to: '+60145586061',
+      from: '+60145586061',
+      body: 'Hello There'
+    )
+  end
+  
+  test "should default to open" do
+    assert Conversation.create!(platform: 'sms')
+  end
+
+  test "should follow messages platform" do
+    @messenger.save!
+    @sms.save!
+    @whatsapp.save!
+    assert @messenger.platform == @messenger.conversation.platform
+    assert @sms.platform == @sms.conversation.platform
+    assert @whatsapp.platform == @whatsapp.conversation.platform
+  end
 end
